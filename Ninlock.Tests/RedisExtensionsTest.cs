88faa6ca2161c97dae19a53967209253318eb3f4
cellistlondon <cthomas.london@gmail.com>
2015-03-01 23:26:08 +0000

    Initial commit

---
+++ Ninlock.Tests/RedisExtensionsTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.Generic;
+using StackExchange.Redis;
+using Ninlock;
+using Moq;
+using System.Diagnostics;
+
+namespace Ninlock.Tests
+{
+    [TestClass]
+    public class RedisExtensionsTest
+    {
+        [TestMethod]
+        public async Task BasicLockTestAsync()
+        {
+            ConnectionMultiplexer redis = await ConnectionMultiplexer.ConnectAsync("localhost");
+            IDatabaseAsync db = redis.GetDatabase();
+            int counter = 0;
+            Func<Task> t1 = async () =>
+            {
+                using (await db.AcquireLockAsync("Lock2",TimeSpan.FromSeconds(2)))
+                {
+                    //await Task.Delay(TimeSpan.FromSeconds(0.1));
+                    Debug.WriteLine("tick1");
+                    counter = counter+1;
+                    Debug.WriteLine("tock1");
+                }
+            };
+            Func<Task> t2 = async () =>
+            {
+                using (await db.AcquireLockAsync("Lock2",TimeSpan.FromSeconds(2)))
+                {
+                    Debug.WriteLine("tick2");
+                    counter = counter + 5;
+                    Debug.WriteLine("tock2");
+                }
+            };
+            await Task.WhenAll(t1(), t2());
+            Assert.AreEqual(6, counter);
+        }
+
+    }
+}
---
+++ Ninlock/RedisExtensions.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using StackExchange.Redis;
+using System.Diagnostics;
+
+namespace Ninlock
+{
+    //based on: https://codingsolution.wordpress.com/2013/07/30/distributed-lock-with-appfabric-caching/
+    public static class RedisExtensions
+    {
+        public static Task<IDisposable> AcquireLockAsync(this IDatabaseAsync db, string key, TimeSpan? expiry = null, TimeSpan? retryTimeout = null)
+        {
+            if (db == null)
+            {
+                throw new ArgumentNullException("db");
+            }
+            if (key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+            return DataCacheLock.AcquireAsync(db, key, expiry, retryTimeout);
+        }
+
+        private class DataCacheLock : IDisposable
+        {
+            private static StackExchange.Redis.IDatabaseAsync _db;
+            public readonly RedisKey Key;
+            public readonly RedisValue Value;
+            public readonly TimeSpan? Expiry;
+            private DataCacheLock(IDatabaseAsync db, string key, TimeSpan? expiry)
+            {
+                _db = db;
+                Key = "ninlock:" + key;
+                Value = Guid.NewGuid().ToString();
+                Expiry = expiry;
+            }
+            public static async Task<IDisposable> AcquireAsync(IDatabaseAsync db, string key, TimeSpan? expiry, TimeSpan? retryTimeout)
+            {
+                DataCacheLock dataCacheLock = new DataCacheLock(db, key, expiry);
+                Func<Task<bool>> task = async () =>
+                {
+                    try
+                    {
+                        return await _db.LockTakeAsync(dataCacheLock.Key, dataCacheLock.Value, dataCacheLock.Expiry ?? TimeSpan.MaxValue);
+                    }
+                    catch
+                    {
+                        return false;
+                    }
+                };
+
+                await RetryUntilTrueAsync(task, retryTimeout);
+                return dataCacheLock;
+            }
+            public void Dispose()
+            {
+                Debug.WriteLine("release the lock:" + Value);
+                _db.LockReleaseAsync(Key, Value).Wait();
+            }
+        }
+        private static readonly Random _random = new Random();
+        private static async Task<bool> RetryUntilTrueAsync(Func<Task<bool>> task, TimeSpan? retryTimeout)
+        {
+            int i = 0;
+            DateTime utcNow = DateTime.UtcNow;
+            while (!retryTimeout.HasValue || DateTime.UtcNow - utcNow < retryTimeout.Value)
+            {
+                i++;
+                if (await task())
+                {
+                    return true;
+                }
+                var waitFor = _random.Next((int)Math.Pow(i, 2), (int)Math.Pow(i + 1, 2) + 1);
+                await Task.Delay(waitFor);
+            }
+            throw new TimeoutException(string.Format("Exceeded timeout of {0}", retryTimeout.Value));
+        }
+
+    }
+}
+
